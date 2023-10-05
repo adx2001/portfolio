9202d6010ec5185e539c922d844314c9b64158c8
Adithyan R <adithyanadhi141@gmail.com>
2023-10-05 20:01:53 +0530

    mm

--- scripts.js
+++ scripts.js
@@ -4,18 +4,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=>{
-    console.log(entry)
-    if(entry.isIntersecting){
-      entry.target.classList.add('show')
-    }else{
-      entry.target.classList.remove('show')
-
-    }
-  })
-})
-
-
-const hiddenElements=document.querySelectorAll('.about');
-hiddenElements.forEach((el)=>observer.observe(el));
